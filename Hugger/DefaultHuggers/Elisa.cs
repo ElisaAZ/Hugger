5640f7674b20a03c5160c7aa1ac336d67ab30c1c
Elisa Avila <elisa.avila@headspring.com>
2020-05-21 13:41:49 -0500

    Adds new Hug class.

---
+++ Hugger/DefaultHuggers/Elisa.cs
@@ -0,0 +1,22 @@
+﻿namespace Hugger.DefaultHuggers
+{
+    public sealed class Elisa : IHugger
+    {
+        private const string Description = "Social distancing until further notice";
+        private readonly IHug _hug;
+
+        public Elisa()
+        {
+            _hug = new Hug
+            {
+                Hugger = this,
+                Description = Description,
+            };
+        }
+
+        public IHug Hug()
+        {
+            return _hug;
+        }
+    }
+}
